22c687550746d2fa7dc8ed35a95f39371d7d340a
Tom Pullen <tpullen21@gmail.com>
2018-07-26 12:52:14 +0100

    Added post and delete

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -15,12 +15,19 @@ class FullPost extends Component {
             if (this.state.loadedPost && this.state.loadedPost.id === this.props.id) {
                 return;
             }
-            
+
             axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
                 .then(response => {
                     this.setState({loadedPost: response.data});
                 });
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -36,7 +43,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
     
