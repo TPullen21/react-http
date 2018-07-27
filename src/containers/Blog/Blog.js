36455640c2c7ac53f27c4ba610ddbd243ac0118f
Tom Pullen <tpullen21@gmail.com>
2018-07-27 09:55:43 +0100

    Render component for a route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import Posts from './Posts/Posts';
+import {Route} from 'react-router-dom';
 
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
@@ -18,7 +19,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
